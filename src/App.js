e674100f86f751d3de978f1bf3c340b1f0be3a24
Priyanka-Pol <priyankapol2019@gmail.com>
2023-06-17 18:08:07 +0530

    signup form react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    // Validate form inputs
+    const newError = [];
+    if (!name || !email || !password || !confirmPassword) {
+      newError.push('All fields are mandatory');
+    }
+
+    if (password !== confirmPassword) {
+      newError.push('Passwords do not match');
+    }
+    setError(newError);
+
+    
+    // Show success message
+    setSuccess('Signup successful');
+
+}
+
+ return (
+    <div className='signup-container'>
+      <h1>Signup</h1>
+      
+      <input
+        type="text"
+        placeholder="Full Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      {error && <div className="error">{error}</div>}
+      {success && <div className="success">{success}</div>}
+      <button onClick={handleSignup}>Signup</button>
     </div>
   );
 }
+
 
 export default App;
